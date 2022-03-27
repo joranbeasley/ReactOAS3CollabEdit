b2c6ca17d6622bb248b6bf2b6697ad6835cf98b7
joran <joran@decagon.com>
2022-03-26 21:44:10 -0700

    ci: fixing py ci
    add importlib_metadata backport to support python3.7

--- backend-server-twisted/acejs_collab_server/__version__.py
+++ backend-server-twisted/acejs_collab_server/__version__.py
@@ -1,10 +1,10 @@
-import importlib.metadata
+import importlib_metadata
 __name__ = "oas3_collab_websocketserver"
 
 import os
 
 try:
-    __version__ = importlib.metadata.version(__name__)
+    __version__ = importlib_metadata.version(__name__)
 except:
     import re
     print("Warning package is running, but not currently installed you should install the pip package")
